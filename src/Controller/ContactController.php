a836972bdbb2cecd75b821f3d2a1c2a7e7f456d8
ArchBacon <C.Delicaat@hotmail.com>
2021-09-28 01:46:23 +0200

    Created about and contact controllers

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,13 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+final class ContactController extends AbstractController
 {
-    #[Route('/', name: 'portfolio')]
+    #[Route('/contact', name: 'contact')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
         ]);
     }
 }
