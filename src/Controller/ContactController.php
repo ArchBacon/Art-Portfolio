cae696506af48cca1eddf2335244eaa0d26981f5
ArchBacon <C.Delicaat@hotmail.com>
2021-09-28 02:30:44 +0200

    Added basic contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,33 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+        $message = new Message();
+
+        $form = $this->createForm(ContactType::class, $message);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = $form->getData();
+
+            //TODO: Flush to DB
+
+            return $this->redirectToRoute('portfolio');
+        }
+
+        return $this->renderForm('contact/index.html.twig', [
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Entity/Message.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $Email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $Message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->Message;
+    }
+
+    public function setMessage(string $Message): self
+    {
+        $this->Message = $Message;
+
+        return $this;
+    }
+}
