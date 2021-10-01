038fb10043187e8011f0128269aa5ad3f0886f9d
ArchBacon <C.Delicaat@hotmail.com>
2021-10-01 02:47:46 +0200

    Added basic file upload

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +18,20 @@ final class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    #[Route('/upload', name: 'upload')]
+    public function upload(Request $request): Response
+    {
+        $upload_dir = $this->getParameter('upload_dir');
+
+        // TODO: make chunked file upload
+
+        /** @var UploadedFile $file */
+        foreach ($request->files as $file) {
+            move_uploaded_file($file->getRealPath(), $upload_dir . uniqid() . '.' . $file->getClientOriginalExtension());
+        }
+
+        return new Response();
     }
 }
