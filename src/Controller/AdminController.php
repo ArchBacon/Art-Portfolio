cab319bad268db98813fc09ec1828b2ff5059be4
ArchBacon <C.Delicaat@hotmail.com>
2021-10-01 18:57:38 +0200

    attempted chunked upload

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,21 +15,59 @@ final class AdminController extends AbstractController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/index.html.twig');
     }
 
+    /** @link https://www.plupload.com/docs/v2/Chunking */
     #[Route('/upload', name: 'upload')]
     public function upload(Request $request): Response
     {
+        /** @var UploadedFile $file */
+        $file = $request->files->get('file');
+        $fileName = $request->get('name');
+        $chunk = $request->get('chunk');
+        $chunks = $request->get('chunks');
         $upload_dir = $this->getParameter('upload_dir');
+        $gallery = $this->getParameter('gallery');
 
-        // TODO: make chunked file upload
+        $tmpFile = fopen($upload_dir . $fileName . '.blob_' . $chunk, 'wb');
+        if ($tmpFile === false) {
+            // TODO: Something went wrong. Couln't open or create file.
+        }
+
+        fwrite(realpath($tmpFile), fopen($file->getRealPath(), 'rb'));
+        fclose($tmpFile);
+
+        return new Response();
+
+        rename($file->getRealPath(), $upload_dir . $fileName . '.blob.' . $chunk);
 
         /** @var UploadedFile $file */
         foreach ($request->files as $file) {
-            move_uploaded_file($file->getRealPath(), $upload_dir . uniqid() . '.' . $file->getClientOriginalExtension());
+            $out = fopen("{$upload_dir}{$request->get('name')}.part", $chunk == 0 ? 'wb' : 'ab');
+            if ($out !== false) {
+                $in = fopen($file->getRealPath(), 'rb');
+                if ($in !== false) {
+                    while ($buff = fread($in, 4096)) {
+                        fwrite($out, $buff);
+                    }
+                } else {
+                    die('{"OK": 0, "info": "Failed to open input stream."}');
+                }
+
+                fclose($in);
+                fclose($out);
+
+                unlink($file->getRealPath());
+            } else {
+                die('{"OK": 0, "info": "Failed to open output stream."}');
+            }
+
+            if ($chunk === $chunks - 1) {
+                rename(
+                    "{$upload_dir}{$request->get('name')}.part",
+                    $upload_dir . $request->get('name'));
+            }
         }
 
         return new Response();
